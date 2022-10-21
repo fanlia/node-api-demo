2385e80cdaef512ba2c9035b86936756ae942d42
fanlia <you@example.com>
2022-10-21 15:19:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Node-API demo
+
+maybe a starter for a node native addon using node-api
---
+++ test.js
@@ -0,0 +1,4 @@
+
+const binding = require('./build/Release/binding.node')
+
+console.log(binding.doSomethingUseful())
